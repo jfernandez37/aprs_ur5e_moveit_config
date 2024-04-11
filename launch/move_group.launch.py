81b7f60c79d7cb78f981a0ebb90035b8a6d199eb
Joe Fernandez <jothomas0615@gmail.com>
2024-04-11 16:51:53 -0400

    Works for simulation

--- launch/move_group.launch.py
+++ launch/move_group.launch.py
@@ -1,15 +1,22 @@
 from launch import LaunchDescription
 from launch.actions import OpaqueFunction
 
+import os
+
 from launch_ros.actions import Node
 
 from moveit_configs_utils import MoveItConfigsBuilder
 
+from ament_index_python.packages import get_package_share_directory
+
 def launch_setup(context, *args, **kwargs):
+    
+    urdf = os.path.join(get_package_share_directory("aprs_description"), "urdf/aprs_lab_robots.urdf.xacro")
+
     moveit_config = (
-        MoveItConfigsBuilder("ur5e", package_name="aprs_ur5e_moveit_config")
-        .robot_description(file_path="config/ur5e.urdf")
-        .robot_description_semantic(file_path="config/ur5e.srdf")
+        MoveItConfigsBuilder("aprs_ur", package_name="aprs_ur5e_moveit_config")
+        .robot_description(urdf)
+        .robot_description_semantic(file_path="config/aprs_lab_robots.srdf")
         .trajectory_execution(file_path="config/controllers.yaml")
         .planning_pipelines(pipelines=["ompl"])
         .to_moveit_configs()
@@ -21,7 +28,8 @@ def launch_setup(context, *args, **kwargs):
         executable="move_group",
         output="screen",
         parameters=[
-            moveit_config.to_dict()
+            moveit_config.to_dict(),
+            {"use_sim_time": True}
         ],
     )   
 
